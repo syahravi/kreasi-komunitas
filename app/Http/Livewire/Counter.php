2d7369d885634ec572ac2b7ac966c07a11ae0a06
Syahravi <syahravi.id@gmail.com>
2023-06-17 21:02:48 +0700

    test Livewire

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $count = 1;
+
+    function increment(){
+        $this->count++;
+    }
+    function decrement() {
+        $this->count--;
+    }
+    public function render(){
+        return view('livewire.counter');
+    }
+}

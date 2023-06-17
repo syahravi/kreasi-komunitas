f1f0300e02b2ea0663c4f45fc0a18d239f521cfb
Syahravi <syahravi.id@gmail.com>
2023-06-17 22:16:41 +0700

    del

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf_token" value="{{ csrf_token() }}"/>
+        {{-- <meta name="csrf_token" value="{{ csrf_token() }}"/> --}}
 
         <title>Kreasi Komunitas</title>
 

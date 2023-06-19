82301e0377ce82c24bcaa3322382acaa2c39dcac
Ubuntu ABhome <syahravi.id@gmail.com>
2023-06-19 16:09:15 +0700

    database

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -3,16 +3,29 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Semangat;
 
 class Counter extends Component
 {
-    public $count = 4;
+    public $count;
 
+    public function mount()
+    {
+        // Inisialisasi data
+        $this->count = Semangat::where('id', 1)->first()->count;
+    }
     function increment(){
         $this->count++;
+        $count = Semangat::where('id', 1)->first();
+        $count->count = $this->count;
+        $count->save();
     }
     function decrement() {
         $this->count--;
+        $count = Semangat::where('id', 1)->first();
+        $count->count = $this->count;
+        $count->save();
+
     }
     public function render(){
         return view('livewire.counter');

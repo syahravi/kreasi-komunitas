6308258df1cb62366ca85009604a09ad6da8796a
Ubuntu ABhome <syahravi.id@gmail.com>
2023-06-19 14:37:13 +0700

    Jquery dan Common

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,8 @@
 
 		<!-- Vendor -->
 		<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+		<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+		<script src="{{ asset('vendor/common/common.js') }}"></script>
 
 	</body>
 </html>

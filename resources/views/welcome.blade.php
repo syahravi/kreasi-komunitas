9b372b1084f588f982f41f5dbe140d9b75e132a3
Syahravi <syahravi.id@gmail.com>
2023-07-15 22:45:42 +0700

    Revert "Revert "Favicon""
    This reverts commit 989f1ac6280491c40b848e273bd64d47a9961ba3.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,12 @@
     {{-- *Title --}}
 		<title>{{ env('APP_NAME') }}</title>
     {{-- ?Favicon --}}
-    <link rel="shortcut icon" type="image/x-icon" href="{{ Vite::img('profile.jpg') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="/site.webmanifest">
     {{-- ?Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 	</head>

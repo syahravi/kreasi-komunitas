4334b0f9e364aaac666185a1af4e8217ad601401
Syahravi <syahravi.id@gmail.com>
2023-07-16 17:04:32 +0700

    Minify dan Navbar

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('zen', 'auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profil', function(){
+        return view('profile.index');
+    });
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });

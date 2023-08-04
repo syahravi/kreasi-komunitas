2e1fc463252992151338291f0a4fa86ecd43337d
Syahravi <syahravi.id@gmail.com>
2023-08-04 11:06:44 +0700

    v1

--- app/Models/PostComment.php
+++ app/Models/PostComment.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostComment extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, Uuids, SoftDeletes;
     protected $table = "post_comments";
     protected $primaryKey = "id";
     protected $dates = ['delete_at'];
--- app/Models/QuestionComment.php
+++ app/Models/QuestionComment.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class QuestionComment extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, Uuids, SoftDeletes;
     protected $table = "question_comments";
     protected $primaryKey = "id";
     protected $dates = ['delete_at'];

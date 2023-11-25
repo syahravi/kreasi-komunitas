eb14274a7811a8dfb97e0b51c907b44feec90435
Syahravi <cc@syahravi.my.id>
2023-11-25 23:20:47 +0700

    /mini

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome')->name('home');
+Route::view('/mini', 'layouts.mini')->name('app');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)

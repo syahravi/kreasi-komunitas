b45c6b8ef02b550550978e49854d1e342b295863
Syahravi <cc@syahravi.my.id>
2023-12-02 20:30:21 +0700

    del CDN

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,8 +10,6 @@
     />
     {{-- <link rel="stylesheet" href="/css/tailwind.css" /> --}}
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <script src="https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@0.5.x/dist/component.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
   </head>
   <body>
     <div
